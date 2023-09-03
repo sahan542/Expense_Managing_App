d8a0b467fd8bcb4addf437280c71ad962c0fadd1
sahan542 <sahanrashmika542@gmail.com>
2023-09-03 17:01:12 +0530

    Controller page & User Models

--- config/connectDb.js
+++ config/connectDb.js
@@ -5,10 +5,11 @@ const connectDb = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Server Running On ${mongoose.connection.host}`.bgGreen.white);
+    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
   }
 };
 
 module.exports = connectDb;
+
