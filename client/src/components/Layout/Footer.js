a1877a1916bdc42b8811c305e480d55f099625a3
sahan542 <sahanrashmika542@gmail.com>
2023-09-09 18:55:12 +0530

    correction

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light'>
+        <h6 className='text-center'>All rights reserved &copy; Sahan Rashmika Silva</h6>
+    </div>
+  )
+}
+
+export default Footer
